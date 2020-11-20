37e7e4c3b26fc1d7b1d9da1dbb760e6ff068e88f
Katherine <47138336+rocka840@users.noreply.github.com>
2020-11-20 09:06:08 +0100

    Update login-flower.php

--- login-flower.php
+++ login-flower.php
@@ -9,11 +9,20 @@
 // Set session variable
 if(isset($_GET["email"]) && isset($_GET["password"])){
     $_SESSION["email"] = $_GET["email"];
-}
+    $_SESSION["password"] = $_GET["password"];
+} else {
 ?>
 
+    <form action="flower-shop.php" method="get">
+    <div>LOGIN please</div>
+    <input name="email" placeholder="please input your email">
+    <input type="password" name="password" placeholder="password here please">
+    <input type="submit" name="go">
+</form>
 
-
+<?php
+}
+?>
 
 </body>
 </html>
